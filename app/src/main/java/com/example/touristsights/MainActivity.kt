c1324cea7b2d156005b68afaf20c2dc2026a5da7
batora9 <sm.net_minecraft.hacker@outlook.jp>
2025-08-27 17:28:30 +0900

    refactor

--- app/src/main/java/com/example/touristsights/MainActivity.kt
+++ app/src/main/java/com/example/touristsights/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.touristsights
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.touristsights.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
